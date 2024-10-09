d8b0e44222bb8029ea7317a3117a67bedd620e5c
Ariana Jorgensen <ariana@arianajorgensen.com>
2024-10-09 15:06:41 -0700

    adding time feature

--- navbar.js
+++ navbar.js
@@ -1,3 +1,24 @@
+// this function is used to display the current local time for a city
+const showTime = (el) => {
+  fetch('./timezones.json')
+    .then(response => response.json())
+    .then(data => {
+      const map = new Map(Object.entries(data.cities));
+      // could do more data transformation if JSON was larger, avoid long for loop
+      map.forEach((k, v) => {
+        if (k.section === el.target.id) {
+          var date = new Date(Date.now());
+          // parsing to get only the time, not date
+          var dateSplit = date.toLocaleString('en-US', {timeZone: k.timezone}).split(" ");
+          var timeComponent = document.getElementById('time-div');
+          timeComponent.innerHTML = 'Current local time is: ' + dateSplit[1] + ' ' + dateSplit[2];
+          // setting diplay property to show time
+          timeComponent.style.display = 'block';
+        }
+      });
+    })
+    .catch(err => alert('Error retrieving timezone file: ', err));
+};
 // read data from json file
 fetch('./navigation.json')
   .then(response => response.json())
@@ -13,7 +34,7 @@ fetch('./navigation.json')
       nav.appendChild(link);
     }
     // adding nav bar to html
-    document.body.appendChild(nav);
+    document.getElementById('nav-container').appendChild(nav);
   }).then(() => {
     // setting the animation and interaction behaviors
     var menu = document.querySelector("nav");
@@ -21,6 +42,7 @@ fetch('./navigation.json')
     // setting the onclick function for each nav item
     for (let i = 0; i < elements.length; i++) {
       elements[i].onclick = function(el) {
+        showTime(el);
         var tmp = document.getElementsByTagName('a');
         for (let j = 0; j < tmp.length; j++) {
           // removing styling from any previously selected items
@@ -35,4 +57,5 @@ fetch('./navigation.json')
         menu.style.setProperty('--underline-width', el.target.offsetWidth + 'px');
       };
     }
-  });
+  })
+  .catch(err => alert('Error loading the navigation.json file: ', err));
